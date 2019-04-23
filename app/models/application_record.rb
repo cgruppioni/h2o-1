a4b516129e0d97b3eb96277c992bb6019aec558b
Casey Gruppioni <cgruppioni@gmail.com>
2019-04-23 12:21:58 -0400

    Add dump_fixture method to application_record.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -8,4 +8,12 @@ class ApplicationRecord < ActiveRecord::Base
   def user_display
     self.user.nil? ? nil : self.user.display
   end
+
+  def dump_fixture
+    fixture_file = "#{Rails.root}/test/fixtures/#{self.class.table_name}.yml"
+    File.open(fixture_file, "a+") do |f|
+      f.puts({ "#{self.class.table_name.singularize}_#{id}" => attributes }.
+        to_yaml.sub!(/---\s?/, "\n"))
+    end
+  end
 end
