d62ab5cb7374041c3d6df9f5b73e692afb349dfe
Casey Gruppioni <cgruppioni@gmail.com>
2019-06-24 16:09:37 -0400

    Resource export pandoc view

---
+++ lib/html_helpers.rb
@@ -0,0 +1,92 @@
+module HTMLHelpers
+  class << self
+    def strip_comments! html
+      html.xpath('//comment()').remove
+      html
+    end
+
+    def unnest! html
+      html
+        .xpath('//article | //section | //aside')
+        .each { |el| el.replace el.children }
+      html
+    end
+
+    def empty_ul_to_p! html
+      html
+        .xpath('//body/ul[not(*[li])]')
+        .each { |list| list.replace "<p>#{list.inner_html}</p>" }
+      html
+    end
+
+    def wrap_bare_inline_tags! html
+      html
+        .xpath('//body/*[not(self::p|self::center|self::blockquote|self::article)]')
+        .each { |inline_element| inline_element.replace "<p>#{inline_element.to_html}</p>" }
+      html
+    end
+
+    def get_body_nodes_without_whitespace_text html
+      html.xpath "//body/node()[not(self::text()) and not(self::text()[1])]"
+    end
+
+    def filter_empty_nodes! nodes
+      nodes.each do |node|
+        if ! node.nil? && node.children.empty?
+          nodes.delete(node)
+        end
+      end
+      nodes
+    end
+
+    def process_p_nodes html
+      [method(:strip_comments!),
+       method(:unnest!),
+       method(:empty_ul_to_p!),
+       method(:wrap_bare_inline_tags!),
+       method(:get_body_nodes_without_whitespace_text),
+       method(:filter_empty_nodes!)].reduce(html) { |memo, fn| fn.call(memo) }
+    end
+
+    def prep_for_pandoc html
+
+      # Case Header styling
+      html.css(
+          # 'section.head-matter p',
+          # 'section.head-matter h4',
+          'center',
+          'p[style="text-align:center"]',
+          'p[align="center"]',
+          ).each do | p |
+        p.wrap("<div data-custom-style='Case Header'></div>")
+      end
+
+      # Note and Link annotations
+      html.css('span[msword-style="FootnoteReference"]').each do | s |
+        s.set_attribute('custom-style', 'Footnote Reference')
+        s.delete('msword-style')
+      end
+
+      # Elision annotations
+      html.css('span.annotate.elided').each do | s |
+        if s['class'].include? 'head'
+          s.inner_html = '[ … ]'
+          s.set_attribute('custom-style', 'Elision')
+        else
+          s.remove
+        end
+      end
+
+      # Replacement annotations
+      html.css('span.annotate.replaced').each do | s |
+        s.remove
+      end
+      html.css('span[msword-style="ReplacementText"]').each do | s |
+        s.set_attribute('custom-style', 'Replacement Text')
+        s.delete('msword-style')
+      end
+
+      html
+    end
+  end
+end
