d67e3ce4b182b0c7aad1eb4f9d28d8f0798e43da
Casey Gruppioni <cgruppioni@gmail.com>
2019-06-27 11:32:09 -0400

    Resources test is a model test, instead of outdated controller test.

--- test/models/content/resources_test.rb
+++ test/models/content/resources_test.rb
@@ -1,16 +1,28 @@
 require 'test_helper'
 
-class ResourcesControllerTest < ActiveSupport::TestCase
-  it 'compare SSR to to render to string' do
-    @resource = content_nodes(:public_annotated_casebook_section_1_1)
+class ResourcesTest < ActiveSupport::TestCase
+  before do 
+    @resource = content_nodes(:public_casebook_section_1_1)
     @include_annotations = true
-
-    ssr_html = Vue::SSR.render(@resource.resource.content, @resource.annotations)
-
-    assert_equal HTMLUtils.parse(render_to_string_output).text, HTMLUtils.parse(ssr_html).text
   end
 
+  describe "without annotations" do
+    it 'compare SSR text to render_to_string' do
+      ssr_output = Vue::SSR.render(@resource.resource.content, @resource.annotations)
+      assert_equal HTMLUtils.parse(render_to_string_output).text, HTMLUtils.parse(ssr_output).text
+    end
+
+    it 'compares ssr html to render_to_string' do
+      ssr_output = Vue::SSR.render(@resource.resource.content, @resource.annotations)
+      assert_equal render_to_string_output, ssr_output
+    end
+  end
+
+  # def ssr_output
+  #   "<DIV class=\"case-text\" data-server-rendered=\"true\" data-v-5735cec2><P data-v-5735cec2>This is the body of case 1.</P></DIV>"
+  # end
+
   def render_to_string_output
-    "<!DOCTYPE html>\n<html>\n<body>\n<div class='ResourceNumber' data-custom-style='Resource Number'>1.1</div>\n<div class='ResourceTitle' data-custom-style='Resource Title'>District Case 3</div>\n<div class='ResourceHeadnote' data-custom-style='Resource Headnote'><p>This is the first resource in the casebook.</p></div>\n<div class='opinion'>\n<p><span class=\"annotate note head tail\">This is the body of </span><span msword-style=\"FootnoteReference\" data-exclude-from-offset-calcs=\"true\">*</span>case 3.</p>\n<div><span msword-style='FootnoteReference'>*</span>This case rocks. </div>\n</div>\n\n</body>\n</html>\n"
+    "<!DOCTYPE html>\n<html>\n<body>\n<div class='ResourceNumber' data-custom-style='Resource Number'>1.1</div>\n<div class='ResourceTitle' data-custom-style='Resource Title'>District Case 1</div>\n<div class='ResourceHeadnote' data-custom-style='Resource Headnote'><p>This is the first resource in the casebook.</p></div>\n<div class='opinion'>\n<p>This is the body of case 1.</p>\n</div>\n\n</body>\n</html>\n"
   end
 end
